8aa3d294b6b47dcef940b7c964fb47c1687bba47
Derek Ericson <derek.ericson@gmail.com>
2019-03-28 22:17:02 -0700

    app appears to be functioning as needed.  styling may update but this works for now

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE `burgers_db`;
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES('Dubba Chee', FALSE);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES('Brogrammer Burger', FALSE);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES('Pizza Burger', FALSE);
+
+
+SELECT * FROM burgers;
