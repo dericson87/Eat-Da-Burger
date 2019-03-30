81bd359137c0cd3cda782a591ffd8e868ef3aeae
Derek Ericson <derek.ericson@gmail.com>
2019-03-29 19:23:08 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,19 @@ var mysql = require ('mysql');
 var connection;
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+		host: 'b4e9xxkxnpu2v96i.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    		port:3306,
+		user: 'y0m9nzambzslqrwv',
+		password: 'zxk48w9qeyq7wcvj',
+		database: 'bk3i5p5d04axeh3w'
+
 } else {
 	connection = mysql.createConnection(
 	{
 		host: 'localhost',
+    		port:3306,
 		user: 'root',
-		password: 'zxk48w9qeyq7wcvj',
+		password: '',
 		database: 'burgers_db'
 	});
 
